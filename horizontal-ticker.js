f7d121d23a24cca35689352fa8f6ba5f130ca8c2
Jan Justyn Dela Cruz <blitzersonicz@gmail.com>
2020-09-11 12:40:47 -0400

    centered the scroll ticker

--- horizontal-ticker.js
+++ horizontal-ticker.js
@@ -1,28 +1,30 @@
 function displayFormat(display) {
-    var displayOutput = '';
-    if (display.anonymous == true) {
-      displayOutput = 'Anonymous chipped in $' + display.donationAmt;
-    } else {
-      displayOutput = display.firstName + ' ' + display.lastName.substring(0, 1) + '. chipped in $' + display.donationAmt;
-    }
-  
-    return displayOutput;
+  var displayOutput = '';
+  if (display.anonymous == true) {
+    displayOutput = 'Anonymous chipped in $' + display.donationAmt;
+  } else {
+    displayOutput = display.firstName + ' ' + display.lastName.substring(0, 1) + '. chipped in $' + display.donationAmt;
   }
-  
-  function tickerStyleScroll() {
-  
-    /* Create style element */
-    var css = document.createElement('style');
-    css.type = 'text/css';
-    var styles = `#ticker {
+
+  return displayOutput;
+}
+
+function tickerStyleScroll() {
+
+  /* Create style element */
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  var styles = `#ticker {
         height: 1.1rem;
         overflow: hidden;
         position: relative;
+        text-align: center;
     } 
     
     #ticker ul {
         float: left;
         padding-left: 20px;
+        width: 100%;
         -webkit-animation: ticker 100s cubic-bezier(1, 0, .5, 0) infinite;
         -moz-animation: ticker 100s cubic-bezier(1, 0, .5, 0) infinite;
         -ms-animation: ticker 100s cubic-bezier(1, 0, .5, 0) infinite;
@@ -39,53 +41,53 @@ function displayFormat(display) {
       list-style: none;
     }`;
 
-    //Webkit Keyframes
-    styles+='@-webkit-keyframes ticker {0%   {margin-top: 0;}';
-    var k=1.98;
-    for (let i = 1; i < 51; i++) {
-            styles += (k*i)+'% {margin-top: -'+ i +'rem;}';
-    };
-    styles += '100% {margin-top: 0;}}';
+  //Webkit Keyframes
+  styles += '@-webkit-keyframes ticker {0%   {margin-top: 0;}';
+  var k = 1.98;
+  for (let i = 1; i < 51; i++) {
+    styles += (k * i) + '% {margin-top: -' + i + 'rem;}';
+  };
+  styles += '100% {margin-top: 0;}}';
 
-    //Moz Keyframes
-    styles+='@-moz-keyframes ticker {0%   {margin-top: 0;}';
-    var k=1.98;
-    for (let i = 1; i < 51; i++) {
-            styles += (k*i)+'% {margin-top: -'+ i +'rem;}';
-    };
-    styles += '100% {margin-top: 0;}}';
+  //Moz Keyframes
+  styles += '@-moz-keyframes ticker {0%   {margin-top: 0;}';
+  var k = 1.98;
+  for (let i = 1; i < 51; i++) {
+    styles += (k * i) + '% {margin-top: -' + i + 'rem;}';
+  };
+  styles += '100% {margin-top: 0;}}';
 
-    //MS Keyframes
-    styles+='@-ms-keyframes ticker {0%   {margin-top: 0;}';
-    var k=1.98;
-    for (let i = 1; i < 51; i++) {
-            styles += (k*i)+'% {margin-top: -'+ i +'rem;}';
-    };
-    styles += '100% {margin-top: 0;}}';
+  //MS Keyframes
+  styles += '@-ms-keyframes ticker {0%   {margin-top: 0;}';
+  var k = 1.98;
+  for (let i = 1; i < 51; i++) {
+    styles += (k * i) + '% {margin-top: -' + i + 'rem;}';
+  };
+  styles += '100% {margin-top: 0;}}';
 
-    //Keyframes
-    styles+='@keyframes ticker {0%   {margin-top: 0;}';
-    var k=1.98;
-    for (let i = 1; i < 51; i++) {
-            styles += (k*i)+'% {margin-top: -'+ i +'rem;}';
-    };
-    styles += '100% {margin-top: 0;}}';
+  //Keyframes
+  styles += '@keyframes ticker {0%   {margin-top: 0;}';
+  var k = 1.98;
+  for (let i = 1; i < 51; i++) {
+    styles += (k * i) + '% {margin-top: -' + i + 'rem;}';
+  };
+  styles += '100% {margin-top: 0;}}';
 
-    if (css.styleSheet)
-      css.styleSheet.cssText = styles;
-    else
-      css.appendChild(document.createTextNode(styles));
-  
-    /* Append style to the head element */
-    document.getElementsByTagName("head")[0].appendChild(css);
-  }
-  
-  function tickerStyleNews() {
-  
-    /* Create style element */
-    var css = document.createElement('style');
-    css.type = 'text/css';
-    var styles = `
+  if (css.styleSheet)
+    css.styleSheet.cssText = styles;
+  else
+    css.appendChild(document.createTextNode(styles));
+
+  /* Append style to the head element */
+  document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+function tickerStyleNews() {
+
+  /* Create style element */
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  var styles = `
       #ticker-wrap {
           width: 100%;
           overflow: hidden;
@@ -147,67 +149,67 @@ function displayFormat(display) {
           }
         }
       `;
-  
-    if (css.styleSheet)
-      css.styleSheet.cssText = styles;
-    else
-      css.appendChild(document.createTextNode(styles));
-  
-    /* Append style to the head element */
-    document.getElementsByTagName("head")[0].appendChild(css);
+
+  if (css.styleSheet)
+    css.styleSheet.cssText = styles;
+  else
+    css.appendChild(document.createTextNode(styles));
+
+  /* Append style to the head element */
+  document.getElementsByTagName("head")[0].appendChild(css);
+}
+
+function appendScrollTicker(donation) {
+  tickerStyleScroll();
+  var ticker = document.querySelector('#donor-ticker');
+  var str = '<ul>'
+  for (var i = 0; i < 50; i++) {
+    str += '<li>' + displayFormat(donation[i]) + '</li>';
   }
-  
-  function appendScrollTicker(donation) {
-    tickerStyleScroll();
-    var ticker = document.querySelector('#donor-ticker');
-    var str = '<ul>'
-    for (var i = 0; i < 50; i++) {
-        str += '<li>' + displayFormat(donation[i]) + '</li>';
-    }
-    str = '<div id="ticker">' + str + '</ul></div>';
-    console.log(str);
-    ticker.innerHTML += str;
+  str = '<div id="ticker">' + str + '</ul></div>';
+  console.log(str);
+  ticker.innerHTML += str;
+}
+
+function appendNewsTicker(donation) {
+  tickerStyleNews();
+  var ticker = document.querySelector('#donor-ticker');
+  var str = '<div class="ticker">'
+  for (var i = 0; i < 50; i++) {
+    str += '<div class="ticker__item">' + displayFormat(donation[i]) + '</div>';
   }
-  
-  function appendNewsTicker(donation) {
-    tickerStyleNews();
-    var ticker = document.querySelector('#donor-ticker');
-    var str = '<div class="ticker">'
-    for (var i = 0; i < 50; i++) {
-      str += '<div class="ticker__item">' + displayFormat(donation[i]) + '</div>';
-    }
-    str = '<div id="ticker-wrap">' + str + '</div></div>';
-    ticker.innerHTML += str;
-  }
-  
-  
-  
-  function displayDonations(donation) {
-    const tickerLayout = document.querySelector('#donor-ticker');
-    console.log(donation);
-  
-    //Ticker layout Type
-    if (tickerLayout.dataset.layout = 'horizontal') {
-      switch (tickerLayout.dataset.style) {
-        case 'news':
-          appendNewsTicker(donation);
-          break;
-        case 'scroll':
-          appendScrollTicker(donation);
-          break;
-        default:
-          appendNewsTicker(donation);
-          break;
-      }
+  str = '<div id="ticker-wrap">' + str + '</div></div>';
+  ticker.innerHTML += str;
+}
+
+
+
+function displayDonations(donation) {
+  const tickerLayout = document.querySelector('#donor-ticker');
+  console.log(donation);
+
+  //Ticker layout Type
+  if (tickerLayout.dataset.layout = 'horizontal') {
+    switch (tickerLayout.dataset.style) {
+      case 'news':
+        appendNewsTicker(donation);
+        break;
+      case 'scroll':
+        appendScrollTicker(donation);
+        break;
+      default:
+        appendNewsTicker(donation);
+        break;
     }
   }
-  
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var myObj = JSON.parse(this.responseText);
-      displayDonations(myObj['donations']);
-    }
-  };
-  xmlhttp.open("GET", "donations.json", true);
-  xmlhttp.send();
+}
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    displayDonations(myObj['donations']);
+  }
+};
+xmlhttp.open("GET", "donations.json", true);
+xmlhttp.send();
