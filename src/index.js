5f464b1f617bd7256c20183da2721aa5fa5ad167
Fernando Santos <fern@ndo.io>
2023-09-14 16:02:37 -0400

    Exposing window.DonorScroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import DonorScroll from "./app/app";
 import "./scss/main.scss";
 //run();
+window.DonorScroll = DonorScroll;
 window.addEventListener("load", function () {
-  window.DonorScroll = DonorScroll;
   let donorScroll = new DonorScroll();
   // Set default options
   // if (typeof window.DonationLightboxOptions !== "undefined") {
